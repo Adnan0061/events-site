51307e3acdbbd4604c88d197d4ecaf951431e878
Adnan Ahmed <adnan.ahmed0061@gmail.com>
2022-01-13 13:32:06 +0600

    5th part of pomodoro

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,10 +1,26 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { Fragment } from 'react'
+import { getEventById } from '../../dummy-data'
+import EventSummary from '../../components/EventDetail/EventSummary'
+import EventLogistics from '../../components/EventDetail/EventLogistics'
+import EventContent from '../../components/EventDetail/EventContent'
 
 function EventDetailPage() {
+    const router = useRouter()
+    const eventId = router.query.eventId
+    const event = getEventById(eventId)
+
+    if(!event) {
+        <p>no event found</p> 
+    }
     return (
-        <div>
-            <h2>event details page</h2>   
-        </div>
+        <Fragment>
+            <EventSummary title={event?.title} />
+            <EventLogistics date={event?.date} address={event?.location} image={event?.image} imageAlt={event?.imageAlt} />
+            <EventContent>
+                <p>{event?.description}</p>
+            </EventContent>
+        </Fragment>
     )
 }
 
