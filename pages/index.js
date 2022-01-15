cac37052c24de249ed9af761d613585110031672
Adnan Ahmed <adnan.ahmed0061@gmail.com>
2022-01-15 12:35:13 +0600

    loading data from db

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import EventList from '../components/EventList';
-import { getFeaturedEvents } from '../dummy-data'
+import { getFeaturedEvents } from '../helpers/api-util'
 
-function HomePage () {
-    const featuredEvents = getFeaturedEvents();
-
+function HomePage (props) {
+    // console.log(props.events)
     return (
         <div>
-            <EventList items={featuredEvents} />
+            <EventList items={props.events} />
         </div>
     )
+}
+
+
+export async function getStaticProps() {
+    const featuredEvents = await getFeaturedEvents()
+    // console.log(featuredEvents)
+    return {
+        props: {
+            events: featuredEvents
+        }
+    }
 }
 
 export default HomePage
