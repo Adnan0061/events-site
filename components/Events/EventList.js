a163a3acfeba20c995ce6fecd0fc0f89a331aa7b
Adnan Ahmed <adnan.ahmed0061@gmail.com>
2024-09-22 13:47:12 +0600

    Update EventList.js with optional chaining

--- components/Events/EventList.js
+++ components/Events/EventList.js
@@ -9,7 +9,7 @@ function EventList({items}) {
         <ul  className={classes.list}>
             {
                 items?.map(event => (
-                    <EventItem key={event.id} title={event.title} image={event.image} date={event.date} location={event.location} id={event.id} />
+                    <EventItem key={event?.id} title={event?.title} image={event?.image} date={event?.date} location={event?.location} id={event?.id} />
                 ))
             }
         </ul>
